ea40161619713da5b6ab4fdad3a75e87e2d6e5b0
Marah-uwase <maranahuwase12@gmail.com>
2020-10-04 21:52:45 +0200

    hide paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,36 @@ $(function () {
     $(devp).hide();
     $(prop).hide();
 
+    $('.des').on('click', function () {
+        $('.des').slideUp('slow');
+        $(desp).show(500);
+        
+    
+    });
+    $('.dev').on('click', function () {
+        $('.dev').slideUp('slow');
+        $(devp).show(500);
+       
+    });
+    $('.pro').on('click', function () {
+        $('.pro').slideUp('slow');
+        $(prop).show(500);
+        
+    });
+
+    $(desp, destext).on('click', function () {
+        $('.des').slideDown();
+        $(desp).hide(500);
+        
+    });
+        $(devp, devtext).on('click', function () {
+            $('.dev').slideDown();
+            $(devp).hide(500);
+            
+        });
+        $(prop, protext).on('click', function () {
+            $('.pro').slideDown();
+            $(prop).hide(500);
+            
+        })
     
