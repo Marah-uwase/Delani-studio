56823f3dbc8eb0cbc69754d524650128c0fe3b52
Marah-uwase <maranahuwase12@gmail.com>
2020-10-04 21:54:23 +0200

    save changes to form

--- js/scripts.js
+++ js/scripts.js
@@ -48,4 +48,22 @@ $(function () {
          }, function(){
             $('.porttext',this).slideToggle('slow');
          });
-    
+
+         $("form#form").on('submit',function(event){
+            event.preventDefault();
+            let name = $("input#name").val();
+            let email = $("input#email").val();
+            let message = $("textarea#message").val();
+
+            if ($("input#name").val() && $("input#email").val()){
+                alert (name + ", we have received your message,Thank you for reaching out to us!:)");
+            }
+            else {
+                alert("Please provide your correct name and email!");
+            }
+
+        });
+
+
+
+});
